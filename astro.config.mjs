e4d44e8e86ef2fecd736aedbba30988ebe40000e
Brayan Smith Cordova Tasayco <gammerpro06@gmail.com>
2026-03-04 12:39:57 -0500

    feat: Integrate Tailwind CSS, add custom fonts and Resend, and establish a base Astro layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [],
+  site: "https://brayan-cordova.vercel.app/",
+  trailingSlash: "always",
+  output: 'static',
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
