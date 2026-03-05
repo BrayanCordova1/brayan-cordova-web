3325d522d4186d72563fba47753c7effe70fe099
Brayan Smith Cordova Tasayco <gammerpro06@gmail.com>
2026-03-04 21:11:40 -0500

    feat: Implement initial project structure including layout (nav & footer components), diverse SVG icons, project images, and data files.

---
+++ src/content/navigation.ts
@@ -0,0 +1,18 @@
+export const navigation = [
+  {
+    slug: '#section-aboutme',
+    label: 'Sobre mi',
+  },
+  {
+    slug: '#section-skill',
+    label: 'Habilidades',
+  },
+  {
+    slug: '#section-projects',
+    label: 'Proyectos',
+  },
+  {
+    slug: '#section-contact',
+    label: 'Contacto',
+  }
+] as const;
---
+++ src/content/redes.ts
@@ -0,0 +1,11 @@
+import Gothib from "@assets/icons/logo/Github.svg"
+import Document from "@assets/icons/Document.svg"
+import LinkedIn from "@assets/icons/logo/Linkedin.svg"
+import Email from "@assets/icons/logo/Email.svg"
+
+export const redes = [
+  { icon: Gothib, href: "https://github.com/BrayanCordova1", label: "GitHub" },
+  { icon: Document, href: "https://drive.google.com/file/d/1gdxkLeqyuxV8LewtQDZoJ_BT15V_5hR4/view?usp=sharing", label: "CV" },
+  { icon: LinkedIn, href: "https://www.linkedin.com/in/brayan-cordova-tasayco/", label: "LinkedIn" },
+  { icon: Email, href: "mailto:brayansmithcordovatasayco@gmail.com", label: "Email" },
+]
