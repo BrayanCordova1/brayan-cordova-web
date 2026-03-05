c565aab7c3a013aba68f72e900593f5648e69840
Brayan Smith Cordova Tasayco <gammerpro06@gmail.com>
2026-03-05 17:57:34 -0500

    feat: Set up AboutMe section, global styles, and core UI components.

---
+++ src/content/me.ts
@@ -0,0 +1,15 @@
+import Spain from "@assets/icons/flag/Spain.svg";
+import ReinoUnido from "@assets/icons/flag/ReinoUnido.svg";
+
+export const idiomas= [
+  {
+    idioma: "Español",
+    nivel: "Nativo",
+    icon: Spain
+  },
+  {
+    idioma: "Ingles",
+    nivel: "A2 (Aprendiendo)",
+    icon: ReinoUnido
+  }
+]
